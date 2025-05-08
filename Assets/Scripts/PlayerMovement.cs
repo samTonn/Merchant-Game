189c7504b77fe65fd74e06d495483d0b5b5ff58b
Samuel Tonn <105612261+samTonn@users.noreply.github.com>
2025-05-07 21:25:58 -0600

    Made movement system
    Used Rigidbody2D.MovePosition(), only x-axis movement, no gravity. (all on purpose)
    Gave the ground a box collider, don't know if it needs it.
    No bugs so far.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10;
+    Rigidbody2D rb;
+    Vector2 direction;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    // Update objects
+    public void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + (direction * speed * Time.deltaTime));
+    }
+
+    // Reading the player's inputs
+    public void Update()
+    {                               //GetAxisRaw stops is either go or stop, getAxis would smooth the movement
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+    }
+
+    
+}
